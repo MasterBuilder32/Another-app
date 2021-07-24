099e1e0910ce14a84f0e93f5592636a7d367a305
MasterBuilder32 <84044177+MasterBuilder32@users.noreply.github.com>
2021-07-24 10:30:08 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+var prediction_1="";
+var prediction_2="";
+Webcam.set({
+    width: 350,
+    height: 350,
+    image_format:'png',
+    png_quality:100
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot(){
+    Webcam.snap(function (data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>'
+    });
+}
+console.log('ml5 version', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/rU9gGUE8f/model.json',modelLoaded);
+function modelLoaded(){
+    console.log("Model Loaded");
+}
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data1="The First Prediction is..."+prediction_1;
+    speak_data2=" And The Second Prediction is..."+prediction_2;
+    var utterThis=new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+    synth.speak(utterThis);
+}
+function check(){
+    img=document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+function gotResult(error,result){
+    if(error)
+    {
+        console.error(error);
+    }
+    else{
+        console.log(result);
+        document.getElementById("result_emotion_name").innerHTML=result[0].label;
+        document.getElementById("result_emotion_name2").innerHTML=result[1].label;
+        prediction_1=result[0].label;
+        prediction_2=result[1].label;
+        speak();
+        if(result[0].label == "wave")
+        {
+            document.getElementById("update_emoji").innerHTML="&#128075;";
+        }
+        if(result[0].label == "ok")
+        {
+            document.getElementById("update_emoji").innerHTML="&#128076;";
+        }
+        if(result[0].label == "Thumbs up")
+        {
+            document.getElementById("update_emoji").innerHTML=" &#128077;";
+        }
+        if(result[1].label == "Cool")
+        {
+            document.getElementById("update_emoji2").innerHTML="&#129304;";
+        }
+        if(result[1].label == "thumbs down")
+        {
+            document.getElementById("update_emoji2").innerHTML="&#128078;";
+        }
+        if(result[1].label == "clap")
+        {
+            document.getElementById("update_emoji2").innerHTML="&#128079;";
+        }
+    }
+}
